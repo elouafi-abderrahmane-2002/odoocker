9971b5949d15cc8e1f0b6dc84fad4c86cf5cce2d
elouafi-abderrahmane-2002 <elouafi-abderrahmane-2002@users.noreply.github.com>
2024-09-11 12:00:00 +0100

    Initial commit
    Source: https://github.com/odoocker/odoocker

---
+++ .github/pull_request_template.md
@@ -0,0 +1,22 @@
+### :rocket: Deployment
+- `INIT="my_custom_addon_1"`
+- `UPDATE="my_custom_addon_2"`
+
+### :jigsaw: Odoo/Asana Tasks
+- *Paste Task URL*
+
+### :memo: Changes
+**Change 1: *Title***
+- In detail, explain what changes were made.
+
+*Add links, videos and screenshots here*
+
+<hr>
+
+### :white_check_mark: Code Review Checklist
+- [ ] Comments are present where appropriate and provide useful information, **not just stating what the code is obviously doing**.
+- [ ] No values have been hard-coded where they should be declared as constant.
+- [ ] Successfully passed all tests when implemented.
+- [ ] Install package(s) without additional **WARNINGS** or **ERRORS**.
+- [ ] Update package(s) without additional **WARNINGS** or **ERRORS**.
+- [ ] Uninstall package(s) without additional **WARNINGS** or **ERRORS**.
---
+++ odoo/odoorc.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+# Define the path to the example configuration file
+TEMPLATE_CONF="odoo.conf"
+
+# First pass: Evaluate any nested variables within .env file and export them
+while IFS='=' read -r key value || [[ -n $key ]]; do
+    # Skip comments and empty lines
+    [[ $key =~ ^#.* ]] || [[ -z $key ]] && continue
+    
+    # Removing any quotes around the value
+    value=${value%\"}
+    value=${value#\"}
+    
+    # Evaluate any variables within value
+    eval "value=\"$value\""
+    
+    export "$key=$value"
+done < .env
+
+# Check the USE_REDIS to add base_attachment_object_storage & session_redis to LOAD variable
+if [[ $USE_REDIS == "true" ]]; then
+    LOAD+=",session_redis"
+fi
+
+# Check the USE_REDIS to add attachment_s3 to LOAD variable
+if [[ $USE_S3 == "true" ]]; then
+    LOAD+=",base_attachment_object_storage"
+    LOAD+=",attachment_s3"
+fi
+
+# Check the USE_REDIS to add sentry to LOAD variable
+if [[ $USE_SENTRY == "true" ]]; then
+    LOAD+=",sentry"
+fi
+
+# Copy the example conf to the destination to start replacing the variables
+cp "$TEMPLATE_CONF" "$ODOO_RC"
+
+# Second pass: Replace the variables in $ODOO_RC
+while IFS='=' read -r key value || [[ -n $key ]]; do
+    # Skip comments and empty lines
+    [[ $key =~ ^#.* ]] || [[ -z $key ]] && continue
+    
+    value=${!key} # Get the value of the variable whose name is $key
+    
+    # Escape characters which are special to sed
+    value_escaped=$(echo "$value" | sed 's/[\/&]/\\&/g')
+    
+    # Replace occurrences of the key with the value in $ODOO_RC
+    sed -i "s/\${$key}/${value_escaped}/g" "$ODOO_RC"
+done < .env
+
+echo "Configuration file is generated at $ODOO_RC"
